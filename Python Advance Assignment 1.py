96bf2446bf6342af0434a6b79a22aa6b098878c0
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-05-04 08:06:24 +0530

    Add files via upload

---
+++ Python Advance Assignment 1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the purpose of Python's OOP?
+# 
+# 
+# 
+# Q2. Where does an inheritance search look for an attribute?
+# 
+# 
+# 
+# Q3. How do you distinguish between a class object and an instance object?
+# 
+# 
+# 
+# Q4. What makes the first argument in a class’s method function special?
+# 
+# 
+# 
+# Q5. What is the purpose of the __init__ method?
+# 
+# 
+# 
+# Q6. What is the process for creating a class instance?
+# 
+# 
+# 
+# Q7. What is the process for creating a class?
+# 
+# 
+# 
+# Q8. How would you define the superclasses of a class?
+
+# Q1. What is the purpose of Python's OOP?
+# Sol: OOPs in Python is a programming approach that focuses on using objects and classes as same as other general programming languages. The objects can be any real-world entities. Python allows developers to develop applications using the OOPs approach with the major focus on code reusability. It is very easy to create classes and objects in Python.
+Q.2 Where does an inheritance search look for an attribute?
+Sol: inheritance search is simply a search of the tree from bottom to top looking for the lowest occurrence of an attribute name.Q3. How do you distinguish between a class object and an instance object?
+
+Sol: I will explain it in the simplest way possible: Say you have 5 apples in your basket. Each of those apples is an object of type Apple, which has some characteristics (i.e. big, round, grows on trees).
+
+In programming terms, you can have a class called Apple, which has variables size:big, shape:round, habitat:grows on trees. To have 5 apples in your basket, you need to instantiate 5 apples. Apple apple1, Apple apple2, Apple apple3 etc....
+# Q4. What makes the first argument in a class’s method function special?
+# Sol: First argument is the class itself which is passed implicitly. Always return a valid object from __new__() . Not mandatory, but its main use is to create and return an object.
+# 
+Q5. What is the purpose of the __init__ method?
+Sol: 
+"__init__" is a reseved method in python classes. It is called as a constructor in object oriented terminology. This method is called when an object is created from a class and it allows the class to initialize the attributes of the class.Q6. What is the process for creating a class instance?
+Sol: An instance, in object-oriented programming (OOP), is a specific realization of any object. An object may be varied in a number of ways. In languages that create objects from classes, an object is an instantiation of a class. That's is, it is a member of a given class that has specified values rather than variables.
+Q7. What is the process for creating a class?
+Sol: Create a Class. To create a class, use the keyword class :...
+Create Object. Now we can use the class named MyClass to create objects: ... Q8. How would you define the superclasses of a class?
+Sol: Python super() The super() builtin returns a proxy object (temporary object of the superclass) that allows us to access methods of the base class. In Python, super() has two major use cases: Allows us to avoid using the base class name explicitly. Working with Multiple Inheritance.
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
